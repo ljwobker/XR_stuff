01d08b0d6c0d05868c20ead4cf449d62de99d26e
LJ Wobker <ljwobker@users.noreply.github.com>
2023-03-03 15:13:31 -0500

    Update drop_script.py
    added bundle stuff

--- drop_script.py
+++ drop_script.py
@@ -16,37 +16,6 @@ import subprocess
 import sys
 import time
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%H:%M:%S")
-log = logging.getLogger(__name__)
-
-"""dictionary of commands to run when collecting.  values are lists with the shell executable
-and arguments.  Any XR command can be translated to its shell equivalent with _describe_
-"""
-cmdTable = {
-    "timestamp": ["date", "+%s"],           # XR: "show clock"
-    "showVersion": ["show_version"],        # XR: "show version"
-    "showIntf": ["show_interface", "-a"],   # XR: "show interface"
-    "showInv": ["show_inventory", "-e"],        # XR: show inventory"
-    "etcHostname": ["cat", "/etc/hostname"],        # hostname (no direct XR equiv.)
-    "showNpuSlice": ["show_slicemgr", "-I", "0xff", "-n", "A"],   # XR: show contr npu slice info...
-}
-
-
-# ofa_npu_stats_show "-v" "a" "-i" "0x3" "-n" "768" "-t" "e" "-p" "0xffffffff" "-s" "0x0" "-d" "A"
-
-clearCmdTable = {
-                # these are all auto-generated below...
-}
-
-for card in range(18):
-    for npu_inst in range(4):
-        # this monster is the shell command to run "show controller npu stats" to get all the drops
-        # iterated over each possible LC / NPU combination.  (note: nonexistent LC/NPUs safely return)
-        clearCmdVal = ["npd_npu_driver_clear", "-c", "s", "-i", f"0x{str(npu_inst)}", "-n", f"{str(256*card)}"]
-        cmdval = ["ofa_npu_stats_show", "-v", "a", "-t", "e", "-p", "0xffffffff", "-s", "0x0", "-d", "A",]
-        cmdval = cmdval + ["-i", f"0x{str(npu_inst)}", "-n", f"{str(256*card)}"]
-        cmdTable[f"npu_drops{card}_{npu_inst}"] = cmdval
-        clearCmdTable[f"clear_command_{card}_{npu_inst}"] = clearCmdVal
 
 
 def runShowCommands(cmdTable) -> dict:
@@ -69,33 +38,61 @@ def runShowCommands(cmdTable) -> dict:
             raise TimeoutError("process with handle '{}' timed out!".format(handle))
     return procOutput
 
-def saveJsonXz(cmdOutput: dict, filename: str):
+def saveJsonXz(cmdOutput: dict, filename: str) -> None:
     with lzma.open(filename, "wt", encoding='utf-8') as outfile:
         json.dump(cmdOutput, outfile)
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--time_interval", type=int, default=30, help="seconds between subsequent runs - default 30 sec")
-parser.add_argument("-n", "--num_runs", type=int, default=1, help="number of runs to execute - use '0' to run forever")
-parser.add_argument("-o", "--output_dir", default='/var/xr/disk1/envSnaps', help="output directory, default: /var/xr/disk1/envSnaps")
-parser.add_argument("-l", "--leader", default = '', help="descriptive string prepended to snapshot filenames")
-if len(sys.argv)==1:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
-args = parser.parse_args()
-
+def getParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--time_interval", type=int, default=30, help="seconds between subsequent runs - default 30 sec")
+    parser.add_argument("-n", "--num_runs", type=int, default=1, help="number of runs to execute - use '0' to run forever")
+    parser.add_argument("-o", "--output_dir", default='/var/xr/disk1/envSnaps', help="output directory, default: /var/xr/disk1/envSnaps")
+    parser.add_argument("-l", "--leader", default = '', help="descriptive string prepended to snapshot filenames")
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+    return parser.parse_args()
 
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%H:%M:%S")
+log = logging.getLogger(__name__)
 
+"""dictionary of commands to run when collecting.  values are lists with the shell executable
+and arguments.  Any XR command can be translated to its shell equivalent with _describe_
+"""
+cmdTable = {
+    "timestamp": ["date", "+%s"],           # XR: "show clock"
+    "showVersion": ["show_version"],        # XR: "show version"
+    "showIntf": ["show_interface", "-a"],   # XR: "show interface"
+    "showInv": ["show_inventory", "-e"],        # XR: show inventory"
+    "etcHostname": ["cat", "/etc/hostname"],        # hostname (no direct XR equiv.)
+    "showNpuSlice": ["show_slicemgr", "-I", "0xff", "-n", "A"],   # XR: show contr npu slice info...
+    "showPolMapInt": ["qos_ma_show_stats", "-i", "Bundle-Ether21", "-p", "0x1", "-q", "0x2",]
+}
+
+clearCmdTable = {}
+
+for card in range(18):
+    for npu_inst in range(4):
+        # this monster is the shell command to run "show controller npu stats" to get all the drops
+        # iterated over each possible LC / NPU combination.  (note: nonexistent LC/NPUs safely return)
+        # create commands to clear the counters (we run this once at the beginning)
+        clearCmdVal = ["npd_npu_driver_clear", "-c", "s", "-i", f"0x{str(npu_inst)}", "-n", f"{str(256*card)}"]
+        clearCmdTable[f"clear_command_{card}_{npu_inst}"] = clearCmdVal
+        # build the show command for each (LC, NPU) tuple
+        cmdval = ["ofa_npu_stats_show", "-v", "a", "-t", "e", "-p", "0xffffffff", "-s", "0x0", "-d", "A",]
+        cmdval = cmdval + ["-i", f"0x{str(npu_inst)}", "-n", f"{str(256*card)}"]
+        cmdTable[f"npu_drops{card}_{npu_inst}"] = cmdval
 
 
 if __name__ == '__main__':
     os.nice(20)
+    args = getParser()
     snapshot_dir = args.output_dir
+    runs_remaining = args.num_runs
     log.info(f"Using output directory {snapshot_dir}")
     if not os.path.exists(snapshot_dir):
         os.mkdir(snapshot_dir)
-    runs_remaining = args.num_runs
+
     ClearCommandOutput = runShowCommands(clearCmdTable)   # run once, clear counters...
 
     while True:
